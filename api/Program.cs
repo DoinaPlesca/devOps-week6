d593937a46b4a7e1ae2bbef5cd0b87e21c359f59
Doina Plesca <doinitzaplesca@yahoo.com>
2024-03-05 10:46:14 +0100

    sonarscanner

---
+++ api/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using api;
+
+Console.WriteLine("Hello, World!");
+
+
+MathOperations mathOperations = new MathOperations();
+
+Console.WriteLine("Enter two numbers:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum: {mathOperations.Add(a, b)}");
+Console.WriteLine($"Difference: {mathOperations.Subtract(a, b)}");
+Console.WriteLine($"Product: {mathOperations.Multiply(a, b)}");
+
+try
+{
+    Console.WriteLine($"Quotient: {mathOperations.Divide(a, b)}");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
---
+++ tests/NUnit.cs
@@ -0,0 +1,44 @@
+﻿using api;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+
+namespace tests;
+
+[TestFixture]
+public class NUnit
+{
+    [Test]
+    public void Add_TwoIntegers_ReturnsCorrectSum()
+    {
+        MathOperations mathOperations = new MathOperations();
+        int result = mathOperations.Add(5, 7);
+        Assert.AreEqual(12, result);
+    }
+
+    [Test]
+    public void Subtract_TwoIntegers_ReturnsCorrectDifference()
+    {
+        MathOperations mathOperations = new MathOperations();
+        int result = mathOperations.Subtract(10, 3);
+        Assert.AreEqual(7, result);
+    }
+
+    [Test]
+    public void Multiply_TwoIntegers_ReturnsCorrectProduct()
+    {
+        MathOperations mathOperations = new MathOperations();
+        int result = mathOperations.Multiply(4, 5);
+        Assert.AreEqual(20, result);
+    }
+
+    [Test]
+    public void Divide_TwoIntegers_ReturnsCorrectQuotient()
+    {
+        MathOperations mathOperations = new MathOperations();
+        int result = mathOperations.Divide(10, 2);
+        Assert.AreEqual(5, result);
+    }
+
+    
+}
